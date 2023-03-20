63d45147dcb664966dd5b6cdd132aebcb4839901
Patrick Macom <patrickmacom@gmail.com>
2023-03-20 16:02:28 -0400

    updated compress

--- scripts/copy_cdn.js
+++ scripts/copy_cdn.js
@@ -5,7 +5,9 @@ const fs = require('fs')
 const fse = require('fs-extra')
 const dotenv = require('dotenv')
 const { cdnFolder } = require('./utils')
+const createCompress = require('compress-brotli')
 
+const { compress, decompress } = createCompress()
 dotenv.config({ path: path.resolve(__dirname, '../.env') })
 
 function distFolderRelative(folder) {
