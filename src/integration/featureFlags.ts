d2e78ba1435bef81bc51422db622e6934c788f76
Patrick Macom <patrickmacom@gmail.com>
2023-04-16 03:40:46 -0400

    added env var

--- src/integration/featureFlags.ts
+++ src/integration/featureFlags.ts
@@ -5,8 +5,12 @@ import { FF_APPLICATION_NAME } from '../state/types'
 import { callOnce } from "../utils/callOnce"
 
 export const initializeFeatureFlags = callOnce(async () => {
+  if(!process.env.EXPLORER_JSON_PATH){
+    console.log("No EXPLORER_JSON_PATH env var found, skipping feature flags initialization")
+  }
+  
   const ff = await fetchFlags({
-    featureFlagsUrl: 'https://remotecontrolpeople.org',
+    featureFlagsUrl: process.env.EXPLORER_JSON_PATH,
     applicationName: FF_APPLICATION_NAME
   })
   // store.dispatch(setFeatureFlags(ff))
